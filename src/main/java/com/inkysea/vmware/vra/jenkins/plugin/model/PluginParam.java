8c58bfc231234413e74953e8ebca456aa9f3128b
kthieler <Only4github>
2016-02-26 11:05:14 -0500

    Updated for jenkins wiki page

--- src/main/java/com/inkysea/vmware/vra/jenkins/plugin/model/PluginParam.java
+++ src/main/java/com/inkysea/vmware/vra/jenkins/plugin/model/PluginParam.java
@@ -1,12 +1,26 @@
 package com.inkysea.vmware.vra.jenkins.plugin.model;
 
+import hudson.Extension;
+import hudson.Util;
+import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
+import hudson.model.AbstractDescribableImpl;
+
+import hudson.tools.AbstractCommandInstaller;
+import hudson.util.FormValidation;
 import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Logger;
 
 
-public class PluginParam implements Serializable {
+public class PluginParam  extends AbstractDescribableImpl<PluginParam> implements Serializable {
 
     private String serverUrl;
     private String userName;
@@ -15,6 +29,7 @@ public class PluginParam implements Serializable {
     private String blueprintName;
     private boolean waitExec;
 
+    @DataBoundConstructor
     public PluginParam(String serverUrl, String userName, String password, String tenant, String blueprintName, boolean waitExec) {
         this.serverUrl = serverUrl;
         this.userName = userName;
@@ -72,5 +87,117 @@ public class PluginParam implements Serializable {
         }
 
         return true;
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends Descriptor<PluginParam> {
+
+        private static final Logger log;
+
+        static {
+            log = Logger.getLogger(DescriptorImpl.class.getName());
+        }
+
+
+        /**
+         * In order to load the persisted global configuration, you have to
+         * call load() in the constructor.
+         */
+        public DescriptorImpl() {
+            load();
+        }
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // Indicates that this builder can be used with all kinds of project types
+            return true;
+        }
+
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        @Override
+        public String getDisplayName() {
+            return "Provision vRealize Automation Blueprint";
+        }
+
+        public FormValidation doCheckServerUrl(
+                @QueryParameter final String value) {
+
+            String url = Util.fixEmptyAndTrim(value);
+            if (url == null)
+                return FormValidation.error("Please enter the URL for vRealize Automation.");
+
+            if (url.indexOf('$') >= 0)
+                // set by variable, can't validate
+                return FormValidation.ok();
+
+            try {
+                new URL(value).toURI();
+            } catch (MalformedURLException e) {
+                return FormValidation.error("This is not a valid URI");
+            } catch (URISyntaxException e) {
+                return FormValidation.error("This is not a valid URI");
+            }
+
+            return FormValidation.ok();
+        }
+
+        public FormValidation doCheckUserName(
+                @QueryParameter final String value) {
+
+            String url = Util.fixEmptyAndTrim(value);
+            if (url == null)
+                return FormValidation.error("Please enter user name.");
+
+            if (url.indexOf('$') >= 0)
+                // set by variable, can't validate
+                return FormValidation.ok();
+
+            return FormValidation.ok();
+        }
+
+        public FormValidation doCheckPassword(
+                @QueryParameter final String value) {
+
+            String url = Util.fixEmptyAndTrim(value);
+            if (url == null)
+                return FormValidation.error("Please enter password.");
+
+            if (url.indexOf('$') >= 0)
+                // set by variable, can't validate
+                return FormValidation.error("Environment variable cannot be used in password.");
+
+            return FormValidation.ok();
+        }
+
+        public FormValidation doCheckTenant(
+                @QueryParameter final String value) {
+
+            String url = Util.fixEmptyAndTrim(value);
+            if (url == null)
+                return FormValidation.error("Please enter the tenant.");
+
+            if (url.indexOf('$') >= 0)
+                // set by variable, can't validate
+                return FormValidation.ok();
+
+            return FormValidation.ok();
+        }
+
+        public FormValidation doCheckBluePrintName(
+                @QueryParameter final String value) {
+
+            String url = Util.fixEmptyAndTrim(value);
+            if (url == null)
+                return FormValidation.error("Please enter the Blueprint Name.");
+
+            if (url.indexOf('$') >= 0)
+                // set by variable, can't validate
+                return FormValidation.ok();
+
+            return FormValidation.ok();
+        }
+
+
     }
 }
